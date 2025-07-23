cb13c8ef01946e37ab0edacf35a6dfb6f3be8e70
machine <me@alexandros.tech>
2025-07-23 12:40:57 +0300

    better build

--- build-me.sh
+++ build-me.sh
@@ -2,13 +2,9 @@
 # Run it as follows ./build-me.sh linux arm64
 # Run it as follows ./build-me.sh darwin arm64
 
-rm -rf node_modules/
-rm -rf extensions/node_modules/
-rm -rf out*
+rm -rf node_modules/ && rm -rf extensions/node_modules/ && rm -rf out*
 npm i
-npm run compile-build
-yarn gulp minify-vscode-reh-web
-yarn gulp vscode-reh-web-$1-$2-min-ci
+npm run compile && npm run compile-build && yarn gulp minify-vscode-reh-web && yarn gulp vscode-reh-web-$1-$2-min-ci
 
 mkdir ../output
 mv ../vscode-reh-* ../output
